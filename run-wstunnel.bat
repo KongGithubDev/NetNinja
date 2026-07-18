ffd885d097dbf882655d8b5cd8e8684a9ab83ab7
Watcharapong Namsaeng <kongwatcharapongemail@gmail.com>
2026-07-18 23:56:43 +0700

    Add TLS + SNI routing with NET_WEB_SNI support

--- run-wstunnel.bat
+++ run-wstunnel.bat
@@ -1,8 +1,13 @@
 @echo off
+title NetNinja WS-Tunnel Server
+
+if "%PORT%"=="" set PORT=443
+
+set NET_WEB_SNI=kpstore.online,www.kpstore.online,api.kpstore.online,gta-career.com,www.gta-career.com,api.gta-career.com,cdn.gta-career.com,careercity.gta-career.com,n8n.kongwatcharapong.in.th
+
 echo ========================================
 echo   NetNinja WS-Tunnel Server
 echo ========================================
-if "%PORT%"=="" set PORT=443
 echo   Port : %PORT%
 echo   Mode : EMBEDDED SSH
 echo ========================================
